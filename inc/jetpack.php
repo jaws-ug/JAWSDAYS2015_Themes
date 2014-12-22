c53ae4d63210636c319b7a3c7059e9a1633ccc61
Kazue Igarashi <igarashi@digitalcube.jp>
2014-12-22 13:43:19 +0900

    ogp default

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -17,3 +17,17 @@ function jawsdays2015_jetpack_setup() {
 	) );
 }
 add_action( 'after_setup_theme', 'jawsdays2015_jetpack_setup' );
+
+
+/*
+ * original
+ * $image[] = apply_filters( 'jetpack_open_graph_image_default', "http://wordpress.com/i/blank.jpg" );
+ * テーマに /images/my_open_graph_image_default.png を用意しておく
+ */
+
+add_filter( 'jetpack_open_graph_image_default', 'my_open_graph_image_default' );
+
+function my_open_graph_image_default( $image ) {
+	$image = get_stylesheet_directory_uri() . '/images/other/ogp-main.jpg';
+	return $image;
+}
